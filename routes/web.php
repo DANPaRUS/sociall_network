3434269c3c2c3546548f69227d31638151030e82
Dan <52635228+DANPaRUS@users.noreply.github.com>
2021-08-08 16:24:45 +0300

    version2

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\CheckController;
 
 Route::view('/' , 'index');
 
-Route::name('check.')->group(function(){
+Route::name('check.')->prefix('check')->group(function(){
     Route::post('/check' , [CheckController::class,'index']) -> name('index'); 
     Route::post('/check' , [CheckController::class, 'register']) -> name('register');
-    Route::post('/check' , [CheckController::class, 'login']) -> name('login');
+    Route::post('/check' , [CheckController::class, 'login']) -> name('login'); 
 });
